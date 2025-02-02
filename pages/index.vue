fb5f6c5d6e90e35da62eb98e68220e5629a523ac
therohantomar <therohantomar@gmail.com>
2025-02-02 16:31:34 +0530

    Auth configged

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-3xl text-center font-bold md:text-6xl">
+          Fishery  Inventory Management
+        </h1>
+        <p class="text-lg md:text-2xl lg:text-3xl">
+            By Hum Log
+        </p>
+    </main>
+</template>
+<script lang="ts" setup>
+
+definePageMeta({
+    isAlive:true,
+    middleware: 'sidebase-auth'
+})
+
+</script>
