a980496f17574ed981654a3848369c8e0b44a5fd
therohantomar <therohantomar@gmail.com>
2025-02-02 16:30:23 +0530

    Nuxt config properly configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,45 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@sidebase/nuxt-auth']
+  modules: ['@sidebase/nuxt-auth','@nuxtjs/tailwindcss'],
+  runtimeConfig:{
+    public: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      }
+    }
+  }
+  ,
+  auth: {
+    isEnabled: true,
+    disableServerSideAuth: false,
+    originEnvKey: 'AUTH_ORIGIN',
+    baseURL: 'http://localhost:3000/api/auth',
+		provider: {
+			type: "authjs",
+		},
+  	},
+    tailwindcss: {
+      config: {
+        theme: {
+          extend: {
+        
+          }
+        }
+      }
+    }
 })
