a86c1fb79185c26f2ac7c5f51bbb25533fbb7803
therohantomar <therohantomar@gmail.com>
2025-02-11 23:57:33 +0530

    new typo added

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -1,10 +1,24 @@
+import { Timestamp } from "firebase/firestore"
+
 declare global {
     type Vendors={
+        id:string,
         name:string,
         email:string,
         phone:string,
         plan:'monthly' | '3months' | 'yearly'
         address?:string,
-        locality:string
+        locality:string,
+        dateOfJoining:Timestamp,
+        dateOfLastRenewal:Timestamp
+    }
+    type Fisherman={
+        id:string,
+        name:string,
+        vendorsId:string,
+        dateOfJoining:Timestamp,
+        phone:string,
+        email?:string,
+        isActive:boolean
     }
 }
