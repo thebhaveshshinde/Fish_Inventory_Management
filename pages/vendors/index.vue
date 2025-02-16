434f765fae04d4a398699dbc3bef55b9491a24f6
therohantomar <therohantomar@gmail.com>
2025-02-17 00:15:34 +0530

    compo things

---
+++ pages/vendors/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="flex flex-col items-center pt-20 justify-center">
+        <h1 class="text-3xl font-bold text-gray-700">Hello Vendors </h1>
+        <p class="text-lg text-gray-600">Apan idhar fishermans ko add karte hain</p>
+    </main>
+</template>
