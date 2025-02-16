27cc0c08fa4c4df9fdc8a68823a20f62e2e253c0
therohantomar <therohantomar@gmail.com>
2025-02-17 00:14:33 +0530

    config things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@sidebase/nuxt-auth', '@nuxt/ui','@nuxtjs/google-fonts'],
-  runtimeConfig:{
+  modules: ["@sidebase/nuxt-auth", "@nuxt/ui", "@nuxtjs/google-fonts"],
+  runtimeConfig: {
     firebaseConfig: {
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -11,49 +11,48 @@ export default defineNuxtConfig({
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
     public: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-
-      
-    }
-  }
-  ,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+  },
   auth: {
     isEnabled: true,
     disableServerSideAuth: false,
-    originEnvKey: 'AUTH_ORIGIN',
-    baseURL: 'https://fish-inventory-management.vercel.app/api/auth',
-        provider: {
-            type: "authjs",
-        },
-  	},
-    googleFonts: {
-      families: {
-        Roboto: {
-          wght: "100..900",
+    originEnvKey: "AUTH_ORIGIN",
+    baseURL:
+      process.env.ENVIRONMENT === "DEVELOPMENT"
+        ? "http://localhost:3000/api/auth"
+        : "https://fish-inventory-management.vercel.app/api/auth",
+    provider: {
+      type: "authjs",
+    },
+  },
+  googleFonts: {
+    families: {
+      Roboto: {
+        wght: "100..900",
+      },
+    },
+  },
+
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ["Inter var", "system-ui", "sans-serif"],
+            lato: ["Lato", "sans-serif"],
+          },
         },
       },
     },
-
-    tailwindcss: {
-      config: {
-        theme: {
-          extend: {
-            fontFamily: {
-              sans: ['Inter var', 'system-ui', 'sans-serif'],
-              lato: ['Lato', 'sans-serif']
-            }
-          }
-        }
-      }
-    },
-   
-})
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,13 @@
 module.exports = {
-    content: {
-        files: [
-          // all directories and extensions will correspond to your Nuxt config
-          "{srcDir}/components/**/*.{vue,js,jsx,mjs,ts,tsx}",
-          "{srcDir}/layouts/**/*.{vue,js,jsx,mjs,ts,tsx}",
-          "{srcDir}/pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
-          "{srcDir}/plugins/**/*.{js,ts,mjs}",
-          "{srcDir}/composables/**/*.{js,ts,mjs}",
-          "{srcDir}/utils/**/*.{js,ts,mjs}",
-          "{srcDir}/{A,a}pp.{vue,js,jsx,mjs,ts,tsx}",
-          "{srcDir}/{E,e}rror.{vue,js,jsx,mjs,ts,tsx}",
-          "{srcDir}/app.config.{js,ts,mjs}",
-          "{srcDir}/app/spa-loading-template.html"
-        ]
-      },
+  content: {
+    files: ["./pages/**/*.{vue,js,ts}", "./components/**/*.{vue,js,ts}"],
+  },
   theme: {
     extend: {
       colors: {
-        primary: '#FFD700',
-      }
+        primary: "#FFD700",
+      },
     },
   },
   plugins: [],
-}
+};
