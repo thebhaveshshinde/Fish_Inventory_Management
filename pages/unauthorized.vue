4af18d9dcd7a666ab8936b13509a9347b802fdc2
therohantomar <therohantomar@gmail.com>
2025-02-17 23:24:18 +0530

    added new page

---
+++ pages/unauthorized.vue
@@ -0,0 +1,8 @@
+<template>
+    <main class="flex flex-col items-center pt-20 text-gray-900 justify-center">
+        <h4 class="text-xl md:text-2xl">
+            You Are Unauthorized Kindly contact @admin
+        </h4>
+    </main>
+</template>
+<script lang="ts" setup></script>
