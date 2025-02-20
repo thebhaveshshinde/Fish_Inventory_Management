665eb2d278bfbe827ef8d046dc1b2a55bcc56275
therohantomar <therohantomar@gmail.com>
2025-02-20 18:22:49 +0530

    vue fire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@sidebase/nuxt-auth", "@nuxt/ui", "@nuxtjs/google-fonts"],
+  modules: [
+    "@sidebase/nuxt-auth",
+    "@nuxt/ui",
+    "@nuxtjs/google-fonts",
+    "nuxt-vuefire",
+  ],
   runtimeConfig: {
     firebaseConfig: {
       apiKey: process.env.FIREBASE_API_KEY,
@@ -53,6 +58,18 @@ export default defineNuxtConfig({
           },
         },
       },
+    },
+  },
+  vuefire: {
+    enabled: true,
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
 });
