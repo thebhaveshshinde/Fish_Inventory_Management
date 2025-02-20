ad71399947f883528b9dac2555cd0b13c1750100
therohantomar <therohantomar@gmail.com>
2025-02-20 18:24:01 +0530

    added new constants

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen">
+        <h1 class="text-3xl m-4 text-gray-700 font-bold">
+            Welcome to Fish Inventory Management
+        </h1>
         <NuxtLink to="/admin">
             <h2
-                class="text-2xl bg-gray-900 text-white rounded-md p-4 hover:bg-gray-700 transition-all duration-300"
+                class="text-lg uppercase bg-gray-900 text-white rounded-md p-3 hover:bg-gray-700 transition-all duration-300"
             >
                 Click to Sign In
             </h2>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,5 @@
-export const admins = ["therohantomar@gmail.com", "thebhaveshshinde@gmail.com"];
+export const admins = [
+  "therohantomar@gmail.com",
+  "thebhaveshshinde@gmail.com",
+  "rstomar710@gmail.com",
+];
