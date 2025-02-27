2ff19b53fcaf6585c83980f95eaa0b37b8ce96d7
therohantomar <therohantomar@gmail.com>
2025-02-27 11:57:21 +0530

    formatting

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -1,45 +1,45 @@
-import { Timestamp } from "firebase/firestore";
+import type { Timestamp } from "firebase/firestore";
 
 declare global {
-  type Vendors = {
-    id?: string;
-    name: string;
-    email: string;
-    phone: string;
-    plan: "monthly" | "3months" | "yearly";
-    address?: string;
-    locality: string;
-    dateOfJoining: Timestamp;
-    dateOfLastRenewal: Timestamp;
-  };
-  type Fisherman = {
-    id?: string;
-    name: string;
-    email: string;
-    phoneno: string;
-    address: string;
-    locality: string;
-    boatno: string;
-    accountno: string;
-    upiid: string;
-    vendorsid: string;
-    dateOfJoining: Timestamp;
-    isActive?: boolean;
-  };
-  type Transaction = {
-    id?: string;
-    dateofTransaction: Timestamp;
-    vendorsid: string;
-    fishermanid: string;
-    billstatus: "PAID" | "PENDING";
-    totalamount: number;
-    modeofpayment: "CASH" | "UPI" | "CHEQUE" | "CARD";
-    products: {
-      name: string;
-      quantity: number;
-      typeofquantity: "kg" | "dozens" | "unit";
-      pricedecided: number;
-      totalprice: number;
-    }[];
-  };
+	type Vendors = {
+		id?: string;
+		name: string;
+		email: string;
+		phone: string;
+		plan: "monthly" | "3months" | "yearly";
+		address?: string;
+		locality: string;
+		dateOfJoining: Timestamp;
+		dateOfLastRenewal: Timestamp;
+	};
+	type Fisherman = {
+		id?: string;
+		name: string;
+		email: string;
+		phoneno: string;
+		address: string;
+		locality: string;
+		boatno: string;
+		accountno: string;
+		upiid: string;
+		vendorsid: string;
+		dateOfJoining: Timestamp;
+		isActive?: boolean;
+	};
+	type Transaction = {
+		id?: string;
+		dateofTransaction: Timestamp;
+		vendorsid: string;
+		fishermanid: string;
+		billstatus: "PAID" | "PENDING";
+		totalamount: number;
+		modeofpayment: "CASH" | "UPI" | "CHEQUE" | "CARD";
+		products: {
+			name: string;
+			quantity: number;
+			typeofquantity: "kg" | "dozens" | "unit";
+			pricedecided: number;
+			totalprice: number;
+		}[];
+	};
 }
