b71b4aa54a6f2c93e0eb325f31c81cd9092b23da
Nalavade Nikhil <135808213+NikhilNalavade@users.noreply.github.com>
2025-02-27 18:06:26 +0530

    small ui changes

--- pages/vendors/index.vue
+++ pages/vendors/index.vue
@@ -1,45 +1,104 @@
 <template>
-    <main class="flex flex-col h-screen overflow-y-scroll  pt-20">
-        <div class="flex items-center justify-center space-x-2">
-            <input type="text" placeholder="Search..."
-                class="px-4 py-2 w-64 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500" />
-            <button
-                @click="isFishermanAddFormOpen=true"
-                class="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500">
-                <UIcon name="i-heroicons-plus" class="w-5 h-5" />
-            </button>
+  <main class="flex flex-col h-screen pt-20 overflow-y-scroll">
+    <div class="flex items-center justify-center space-x-2">
+      <input
+        type="text"
+        placeholder="Search..."
+        class="w-64 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500"
+      />
+      <button
+        @click="isFishermanAddFormOpen = true"
+        class="px-4 py-2 text-white bg-teal-500 rounded-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500"
+      >
+        <UIcon name="i-heroicons-plus" class="w-5 h-5" />
+      </button>
+    </div>
+    <div class="flex flex-wrap items-center justify-center gap-4 mt-4">
+      <div
+        v-for="fisherman in fishermans"
+        :key="fisherman.id"
+        class="flex flex-col items-center p-4 bg-white rounded-lg shadow-md w-96"
+      >
+        <h3 class="text-3xl font-bold text-teal-700 capitalize">
+          {{ fisherman?.name }}
+        </h3>
+
+        <span class="font-medium text-teal-600 capitalize"
+          >{{ fisherman?.locality }}
+        </span>
+
+        <div>
+          <p class="mt-2 text-gray-500">
+            Phone:
+            <span class="font-medium text-teal-600">{{
+              fisherman?.phoneno
+            }}</span>
+          </p>
         </div>
-        <div class="flex flex-col items-center justify-center space-y-4 mt-4">
-            <div class="grid grid-cols-1 gap-4 w-full max-w-md">
-                <div v-for="fisherman in fishermans" :key="fisherman.id" class="bg-white shadow-md rounded-lg p-4 flex flex-col items-center">
-                    <h3 class="text-2xl font-bold text-teal-700">{{ fisherman?.name }}</h3>
-                    <p class="text-gray-500 mt-2">Date: {{ new Date(fisherman.dateOfJoining.seconds * 1000).toLocaleDateString() }}</p>
-                    <p class="text-gray-500">Boat No: <span class="font-medium text-teal-600">{{ fisherman?.boatno }}</span></p>
-                    <div class="flex space-x-4 mt-4">
-                        <button class="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600">
-                            <UIcon name="i-heroicons-phone" class="w-5 h-5" />
-                        </button>
-                        <button class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
-                            <UIcon name="i-heroicons-currency-rupee" class="w-5 h-5" />
-                        </button>
-                    </div>
-                </div>
-            </div>
+        <p class="mt-2 text-gray-500">
+          Date:
+          {{
+            new Date(
+              fisherman.dateOfJoining.seconds * 1000
+            ).toLocaleDateString()
+          }}
+        </p>
+        <p class="text-gray-500">
+          Boat No:
+          <span v-if="fisherman?.boatno" class="font-medium text-green-700">
+            {{ fisherman?.boatno }}
+          </span>
+          <span v-else class="font-medium text-gray-500">Not Available</span>
+        </p>
+        <div class="flex justify-center w-full mt-4">
+          <button
+            class="flex items-center gap-2 px-4 py-2 text-white bg-yellow-400 rounded-md hover:bg-yellow-500"
+          >
+            <UIcon name="i-heroicons-currency-rupee" class="w-5 h-5" />
+            <span>Pay Now</span>
+          </button>
         </div>
-    </main>
-    <dialog class="modal bg-black/60" :open="isFishermanAddFormOpen">
-        
-            <div class="modal-box">
-                <div class="flex justify-between items-center mb-4">
-                    <h2 class="text-xl font-semibold">Add Fisherman</h2>
-                    <button @click="isFishermanAddFormOpen = false" class="text-gray-500 hover:text-gray-700">
-                        <UIcon name="i-heroicons-x-mark" class="w-6 h-6" />
-                    </button>
-                </div>
-                <UserAddForm v-if="vendorUser" :vendor="vendorUser"  @close="closeFisherManAddModal()" />
-            </div>
-    
-    </dialog>
+        <div class="flex mt-2 space-x-4">
+          <a
+            :href="`tel:${fisherman.phoneno}`"
+            class="flex items-center gap-2 px-4 py-2 text-white bg-teal-500 rounded-md hover:bg-teal-600"
+          >
+            <UIcon name="i-heroicons-phone" class="w-5 h-5" />
+            <span>Call Now</span>
+          </a>
+          <a
+            :href="`https://wa.me/${fisherman.phoneno}`"
+            target="_blank"
+            class="flex items-center gap-2 px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+          >
+            <UIcon
+              name="i-heroicons-chat-bubble-bottom-center"
+              class="w-5 h-5"
+            />
+            <span>Whatsapp Now</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+  <dialog class="modal bg-black/60" :open="isFishermanAddFormOpen">
+    <div class="modal-box">
+      <div class="flex items-center justify-between mb-4">
+        <h2 class="text-xl font-semibold">Add Fisherman</h2>
+        <button
+          @click="isFishermanAddFormOpen = false"
+          class="text-gray-500 hover:text-gray-700"
+        >
+          <UIcon name="i-heroicons-x-mark" class="w-6 h-6" />
+        </button>
+      </div>
+      <UserAddForm
+        v-if="vendorUser"
+        :vendor="vendorUser"
+        @close="closeFisherManAddModal()"
+      />
+    </div>
+  </dialog>
 </template>
 <script lang="ts" setup>
 import { collection } from "firebase/firestore";
@@ -52,11 +111,11 @@ const isFishermanAddFormOpen = ref(false);
 const vendorUser = ref<Vendors>();
 const stateVendor = useVendors().value;
 vendorUser.value = stateVendor.find(
-	(vendor) => vendor.email === authData.value?.user?.email,
+  (vendor) => vendor.email === authData.value?.user?.email
 );
 
 const closeFisherManAddModal = () => {
-	isFishermanAddFormOpen.value = false;
+  isFishermanAddFormOpen.value = false;
 };
 
 const firestore = useFirestore();
@@ -67,7 +126,7 @@ const { data: fishermanData } = useCollection(fishermanCollection);
 fishermans.value = fishermanData.value as Fisherman[];
 
 definePageMeta({
-	layout: "vendorslayout",
-	middleware: "sidebase-auth",
+  layout: "vendorslayout",
+  middleware: "sidebase-auth",
 });
 </script>
