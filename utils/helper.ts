6eccae1e84b5b2bc4fcf5b5aa104441917e6dd94
Nalavade Nikhil <135808213+NikhilNalavade@users.noreply.github.com>
2025-03-03 16:45:24 +0530

    small fix

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,16 +1,16 @@
 export const checkIfUserExistsInVendors = async (
-	vendors: Vendors[],
-	email: string,
+  vendors: Vendors[],
+  email: string
 ) => {
-	const userExists = vendors.some(
-		(vendor) => vendor?.email?.toLowerCase() === email?.toLowerCase(),
-	);
+  const userExists = vendors.some(
+    (vendor) => vendor?.email?.toLowerCase() === email?.toLowerCase()
+  );
 
-	if (userExists) {
-		navigateTo("/vendors/");
-	} else if (admins.includes(email)) {
-		return;
-	} else {
-		navigateTo("/unauthorized");
-	}
+  if (admins.includes(email)) {
+    return;
+  } else if (userExists) {
+    navigateTo("/vendors/");
+  } else {
+    navigateTo("/unauthorized");
+  }
 };
