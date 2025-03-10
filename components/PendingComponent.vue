37786b52aa519c20dbae42359c26ad9417edcb02
therohantomar <therohantomar@gmail.com>
2025-03-10 18:42:53 +0530

    created two new components for paid and pending bills

---
+++ components/PendingComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="h-full overflow-y-scroll">
+    {{ transactions }}
+  </main>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{ transactions: Transaction[] }>();
+
+const { transactions } = toRefs(props);
+</script>
