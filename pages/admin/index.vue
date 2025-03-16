f9f1f6edc102c3e4b13da3278ac4514d01b56f47
therohantomar <therohantomar@gmail.com>
2025-03-16 12:55:07 +0530

    added export data scene

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col bg-gray-100 py-20 h-full">
+  <main class="flex flex-col bg-gray-100 py-20 min-h-screen">
     <div class="py-10 text-center">
       <h1 class="text-3xl md:text-4xl lg:text-5xl text-gray-600">
         Fishery Management
@@ -11,146 +11,76 @@
     <div class="flex flex-row justify-center space-x-4">
       <div class="grid grid-cols-3 gap-6">
         <div
-          class="bg-gray-600 w-24 cursor-pointer h-24 rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md"
-        >
+          class="bg-gray-600 w-24 cursor-pointer h-24 rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md">
           <span>{{ vendorState.length }}</span>
           <span class="text-sm font-medium">Vendors</span>
         </div>
         <div
-          class="bg-red-600 w-24 h-24 cursor-pointer rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md"
-        >
+          class="bg-red-600 w-24 h-24 cursor-pointer rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md">
           <span>10</span>
           <span class="text-sm font-medium">Issues</span>
         </div>
         <div
-          class="bg-green-600 w-24 h-24 cursor-pointer rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md"
-        >
+          class="bg-green-600 w-24 h-24 cursor-pointer rounded-lg flex flex-col items-center justify-center text-white text-3xl font-bold shadow-md">
           <span>{{ fishermansState.length }}</span>
           <span class="text-sm font-medium">FisherMans</span>
         </div>
       </div>
     </div>
     <div class="flex flex-row justify-center space-x-4 mt-10">
-      <UButton
-        @click="isAddVendorModalOpen = true"
-        color="gray"
-        size="xl"
-        variant="solid"
-        >Add Vendors</UButton
-      >
+      <UButton @click="isAddVendorModalOpen = true" color="gray" size="xl" variant="solid">Add Vendors</UButton>
       <UButton color="teal" size="xl" variant="solid"> Manage Users </UButton>
     </div>
     <dialog class="modal bg-black/60" :open="isAddVendorModalOpen">
-      <div class="modal-box">
+      <div class="bg-gray-900 px-4 py-2 rounded-md">
         <div class="flex items-center justify-between mb-4">
-          <h3
-            class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
-          >
+          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
             Add Vendors
           </h3>
-          <UButton
-            color="gray"
-            variant="ghost"
-            icon="i-heroicons-x-mark-20-solid"
-            class="-my-1"
-            @click="isAddVendorModalOpen = false"
-          />
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+            @click="isAddVendorModalOpen = false" />
         </div>
-        <div class="space-y-4 w-96">
+        <div class="flex flex-col items-center justify-center">
           <!-- Name Input -->
           <div>
-            <label
-              for="name"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-              >Name</label
-            >
-            <input
-              v-model="vendors.name"
-              type="text"
-              id="name"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            />
+            <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Name</label>
+            <input v-model="vendors.name" type="text" id="name" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600" />
           </div>
 
           <!-- Email Input -->
           <div>
-            <label
-              for="email"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-              >Email</label
-            >
-            <input
-              type="email"
-              v-model="vendors.email"
-              id="email"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            />
+            <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Email</label>
+            <input type="email" v-model="vendors.email" id="email" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600" />
           </div>
 
           <!-- Phone Number Input -->
           <div>
-            <label
-              for="phone"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-              >Phone Number</label
-            >
-            <input
-              type="tel"
-              id="phone"
-              v-model="vendors.phone"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            />
+            <label for="phone" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Phone Number</label>
+            <input type="tel" id="phone" v-model="vendors.phone" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600" />
           </div>
 
           <div>
-            <label
-              for="address"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Address</label
-            >
-            <input
-              type="text"
-              id="address"
-              v-model="vendors.address"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            />
+            <label for="address" class="block text-sm font-medium text-gray-700 dark:text-gray-200">
+              Address</label>
+            <input type="text" id="address" v-model="vendors.address" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600" />
           </div>
 
           <div>
-            <label
-              for="locality"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Locality</label
-            >
-            <input
-              type="text"
-              id="locality"
-              v-model="vendors.locality"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            />
+            <label for="locality" class="block text-sm font-medium text-gray-700 dark:text-gray-200">
+              Locality</label>
+            <input type="text" id="locality" v-model="vendors.locality" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600" />
           </div>
 
           <!-- Plan Select Dropdown -->
           <div>
-            <label
-              for="plan"
-              class="block text-sm font-medium text-gray-700 dark:text-gray-200"
-              >Select Plan</label
-            >
-            <select
-              id="plan"
-              v-model="vendors.plan"
-              ref="plan"
-              required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            >
+            <label for="plan" class="block text-sm font-medium text-gray-700 dark:text-gray-200">Select Plan</label>
+            <select id="plan" v-model="vendors.plan" ref="plan" required
+              class="mt-1 block  rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 dark:bg-gray-700 dark:text-white dark:border-gray-600">
               <option value="monthly">Monthly Plan</option>
               <option value="3months">3 Months Plan</option>
               <option value="yearly">Yearly Plan</option>
@@ -159,13 +89,8 @@
 
           <!-- Submit Button -->
           <div class="flex justify-center">
-            <UButton
-              :loading="isDataSubmitting"
-              :disabled="!isVendorsFormvalid"
-              @click="addVendor()"
-              color="indigo"
-              class="mt-4"
-            >
+            <UButton :loading="isDataSubmitting" :disabled="!isVendorsFormvalid" @click="addVendor()" color="indigo"
+              class="mt-4">
               Submit
             </UButton>
           </div>
@@ -215,6 +140,20 @@ const isVendorsFormvalid = computed(() => {
 
 async function addVendor() {
   isDataSubmitting.value = true;
+  const vendorExists = vendorState.value.some(
+    (vendor) =>
+      vendor.name === vendors.value.name && vendor.email === vendors.value.email
+  );
+
+  if (vendorExists) {
+    toast.add({
+      title: "Error",
+      description: "Vendor with the same name and email already exists",
+    });
+    isDataSubmitting.value = false;
+    return;
+  }
+
   const response: { status: number } = await $fetch("/api/addVendors", {
     method: "POST",
     body: {
---
+++ pages/export.vue
@@ -0,0 +1,95 @@
+<template>
+
+    <main class="w-full h-screen pt-10 bg-gray-100">
+        <div class="flex justify-end p-4">
+            <UButton @click="exportCSV()" color="green" class="px-4 py-2">Export CSV</UButton>
+        </div>
+        <UCard class="h-[90vh]">
+            <div class="flex items-center gap-4p-4">
+                <div class="flex items-center space-x-2">
+                    <label class="block text-sm font-medium text-gray-700 dark:text-gray-200">Vendor:</label>
+                    <select @change="getTransactions()" v-model="selectedVendor"
+                        class="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500 dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                        <option value="">All</option>
+                        <option v-for="vendor in vendors" :key="vendor.id" :value="vendor.name">{{ vendor.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="flex items-center justify-center space-x-4 ">
+                    <label class="block text-sm font-medium text-gray-700 dark:text-gray-200">Status:</label>
+                    <select v-model="selectedStatus" @change="getTransactions()"
+                        class="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500 dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                        <option value="">All</option>
+                        <option value="PENDING">PENDING</option>
+                        <option value="PAID">PAID</option>
+                    </select>
+                </div>
+            </div>
+
+            <UTable class="h-[70vh] overflow-y-scroll" :rows="filteredTransactions" :columns="columns">
+                <template #dateoftransaction-data="{ row }">
+                    {{ new Date(row.dateoftransaction?.toDate()).toLocaleString() }}
+                </template>
+            </UTable>
+        </UCard>
+    </main>
+
+</template>
+
+<script setup lang="ts">
+import { collection, getDocs } from "firebase/firestore";
+import { definePageMeta } from "#imports";
+import { useFirestore } from "vuefire";
+import { useVendors } from "~/utils/composables/useVendors";
+
+
+const db = useFirestore()
+const transactions = ref<Transaction[]>([])
+const vendors = ref<Vendors[]>([])
+vendors.value = useVendors().value
+const selectedVendor = ref("")
+const selectedStatus = ref("")
+definePageMeta({
+    middleware: "sidebase-auth",
+    isalive: true
+});
+
+
+
+const columns = [
+    { key: "vendorname", label: "Vendor Name", class: "bg-gray-900 text-white sticky top-0 z-10", sortable: true },
+    { key: "vendoremail", label: "Vendor Email", class: "bg-gray-900 text-white sticky top-0 z-10 text-center", sortable: true },
+    { key: "fishermanname", label: "Fisherman Name", class: "bg-gray-900 text-white sticky top-0 z-10", sortable: true },
+    { key: "fishermanemail", label: "fisherman email", class: "bg-gray-900 text-white sticky top-0 z-10 text-center", sortable: true },
+    { key: "totalamount", label: "Total Amount", class: "bg-gray-900 text-white sticky top-0 z-10 text-center", sortable: true },
+    { key: "dateoftransaction", label: "Date of Transaction", class: "bg-gray-900 text-white sticky top-0 z-10 text-center", sortable: true },
+    { key: "billstatus", label: "Bill Status", class: "bg-gray-900 text-white sticky top-0 z-10 text-center", sortable: true },
+];
+
+
+const getTransactions = async () => {
+    const transactionsCollection = collection(db, "transactions");
+    const transactionsSnapshot = await getDocs(transactionsCollection);
+    const transactionsList = transactionsSnapshot.docs.map(doc => {
+        return {
+            id: doc.id,
+            ...doc.data()
+        } as Transaction
+    });
+    return transactionsList;
+}
+transactions.value = await getTransactions()
+
+
+const filteredTransactions = computed(() => {
+    return transactions.value.filter(transaction => {
+        return (
+            (!selectedVendor.value || transaction.vendorname === selectedVendor.value) &&
+            (!selectedStatus.value || transaction.billstatus === selectedStatus.value)
+        );
+    });
+});
+
+
+</script>
