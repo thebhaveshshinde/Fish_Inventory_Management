38a8b6cdb6d4d4405cc58d44fe0b75e75e37f836
thebhaveshshinde <thebhaveshshinde@gmail.com>
2025-03-28 18:37:42 +0530

    Update Project Name

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen">
         <h1 class="text-3xl m-4 text-gray-700 font-bold">
-            Welcome to Fish Inventory Management
+            Welcome to Fish Vendor and Data Management
         </h1>
         <NuxtLink to="/admin">
             <h2
